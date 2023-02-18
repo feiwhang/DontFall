78408be709731f9990f682d0844e7ba1f21d50f0
feiwhang <feiwhang@gmail.com>
2023-02-18 14:52:26 +0700

    feat: add player & tile sprites

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,6 +6,5 @@ public class SceneController : MonoBehaviour
     public void LoadScene(string scneneName)
     {
         SceneManager.LoadScene(scneneName);
-        Debug.Log($"Scene loaded: {scneneName}");
     }
 }
