e6c4cc21e3326a40432dd33825928c58a37a4111
feiwhang <feiwhang@gmail.com>
2023-02-20 22:13:31 +0700

    fix: change assets

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,8 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    public void LoadScene(string scneneName)
+    public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(scneneName);
+        SceneManager.LoadScene(sceneName);
     }
 }
