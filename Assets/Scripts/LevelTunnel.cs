e64039b66ac3857e46f84eec4eee353acef8f6f8
feiwhang <feiwhang@gmail.com>
2023-02-20 20:59:28 +0700

    feat: move to next level with tunnel

---
+++ Assets/Scripts/LevelTunnel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTunnel : MonoBehaviour
+{
+    [SerializeField] private float levelLoadDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    private IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(levelLoadDelay);
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        var nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
