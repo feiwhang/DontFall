c84c858a5cf8bbfea1fa5a936771951b978e3c73
feiwhang <feiwhang@gmail.com>
2023-02-25 14:54:22 +0700

    feat: add accessory to boost player jump force

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    private const float DefaultJumpForce = 8f;
+    private const float AccessoryJumpForce = 12f;
+    private const float AccessoryEffectDuration = 5f;
     [SerializeField] private float jumpForce = 8f;
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -16,5 +20,17 @@ public class Platform : MonoBehaviour
                 rb.velocity = velocity;
             }
         }
+    }
+    
+    public void OnTouchingAccessory()
+    {
+        jumpForce = AccessoryJumpForce;
+        StartCoroutine(ResetJumpForce());
+    }
+    
+    IEnumerator ResetJumpForce()
+    {
+        yield return new WaitForSeconds(AccessoryEffectDuration);
+        jumpForce = DefaultJumpForce;
     }
 }
